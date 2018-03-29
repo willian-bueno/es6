181ab4344f3ca884db04c489601f1fac83cc1858
willian <willianbueno@outlook.com>
2018-03-29 06:17:51 -0300

    converter de date e método de adicionar pessoa

--- js/app/controllers/PessoaController.js
+++ js/app/controllers/PessoaController.js
@@ -1,5 +1,33 @@
+/**
+ * @author Willian Bueno
+ */
 class PessoaController {
+    constructor(){
+      let selector = document.querySelector.bind(document);  
+      this._inputDataNascimento = selector('#dataNascimento');
+      this._inputIdade = selector('#idade');
+      this._inputSalario = selector('#salario');
+    }
+
+    /**
+     * Adiciona dados de Pessoa
+     * @param {Event} event 
+     */
     adiciona(event) {
-          event.preventDefault();
+      event.preventDefault();
+      let pessoa = new Pessoa(
+        this.converteData(this._inputDataNascimento.value),
+        this._inputIdade.value,
+        this._inputSalario.value
+      )
+    }
+    
+    /**
+     * recebe String formato yyyy-MM-dd e converte para Data
+     * @param {string} data 
+     */
+    converteData(data){
+      console.log(data);
+      return new Date(data.replace(/-/g,','));
     }
   }
